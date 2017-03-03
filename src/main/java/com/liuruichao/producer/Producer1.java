15b37b3a3ef225ae063d05ee2723aefab3fbfee1
liuruichao <liuruichao555@126.com>
2017-03-03 14:11:25 +0800

    sample1

--- src/main/java/com/liuruichao/producer/Producer1.java
+++ src/main/java/com/liuruichao/producer/Producer1.java
@@ -1,5 +1,11 @@
 package com.liuruichao.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 /**
  * Producer1
  *
@@ -7,5 +13,21 @@ package com.liuruichao.producer;
  * Created on 2017/3/3 13:20
  */
 public class Producer1 {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<>("test", "key_" + i, "value_" + i));
+
+        producer.close();
+    }
 }
