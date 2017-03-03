c0f67dc6221c3938cf0f6a8fc6eea252c4e174ae
liuruichao <liuruichao555@126.com>
2017-03-03 13:48:46 +0800

    init project

---
+++ src/main/java/com/liuruichao/consumer/Consumer1.java
@@ -0,0 +1,11 @@
+package com.liuruichao.consumer;
+
+/**
+ * Consumer1
+ *
+ * @author liuruichao
+ * Created on 2017/3/3 13:20
+ */
+public class Consumer1 {
+
+}
---
+++ src/main/java/com/liuruichao/producer/Producer1.java
@@ -0,0 +1,11 @@
+package com.liuruichao.producer;
+
+/**
+ * Producer1
+ *
+ * @author liuruichao
+ * Created on 2017/3/3 13:20
+ */
+public class Producer1 {
+
+}
